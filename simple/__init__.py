65f8b0ddf73275d60f2dea7dd09439cdeffe026f
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-11-19 15:43:16 +0400

    fix simple math problem syntax error

--- simple/__init__.py
+++ simple/__init__.py
@@ -11,7 +11,7 @@ def test_valid_input():
     (check50.run("python3 simple.py")
             .stdin("10")
             .stdin("5")
-            .stdout("10 + 5 = 15\n10 - 5 = 5\n10 * 5 = 50\n10 / 5 = 2.0")
+            .stdout("10 \+ 5 = 15\n10 - 5 = 5\n10 \* 5 = 50\n10 / 5 = 2.0")
             .exit(0))
 
 @check50.check(exists)
@@ -20,7 +20,8 @@ def test_zero_division():
     (check50.run("python3 simple.py")
             .stdin("10")
             .stdin("0")
-            .exit(1))
+            .stdout("", regex=False)
+            .exit(0))
 
 @check50.check(exists)
 def test_negative_numbers():
@@ -28,7 +29,7 @@ def test_negative_numbers():
     (check50.run("python3 simple.py")
             .stdin("6")
             .stdin("3")
-            .stdout("6 + 3 = 9\n6 - 3 = 3\n6 * 3 = 18\n6 / 3 = 2.0")
+            .stdout("6 \+ 3 = 9\n6 - 3 = 3\n6 \* 3 = 18\n6 / 3 = 2.0")
             .exit(0))
 
 @check50.check(exists)
@@ -37,4 +38,5 @@ def test_non_numeric_input():
     (check50.run("python3 simple.py")
             .stdin("abc")
             .stdin("5")
+            .stdout("", regex=False)
             .exit(1))
