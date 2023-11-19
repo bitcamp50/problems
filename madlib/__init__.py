4e38fb703756a3c1cc45339c023399a1e6bcb7c4
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-11-19 13:01:52 +0400

    add exercises 9 and 10

---
+++ madlib/__init__.py
@@ -0,0 +1,50 @@
+import check50
+
+@check50.check()
+def exists():
+    """madlib.py exists"""
+    check50.exists("madlib.py")
+
+@check50.check(exists)
+def test_basic_story():
+    """creates a basic story with noun, verb, adjective, and adverb"""
+    (check50.run("python3 madlib.py")
+            .stdin("dog")
+            .stdin("walk")
+            .stdin("blue")
+            .stdin("quickly")
+            .stdout("Do you walk your blue dog quickly\? That's hilarious!")
+            .exit())
+
+@check50.check(exists)
+def test_different_story():
+    """creates a different story with another set of inputs"""
+    (check50.run("python3 madlib.py")
+            .stdin("cat")
+            .stdin("jump")
+            .stdin("green")
+            .stdin("slowly")
+            .stdout("Do you jump your green cat slowly\? That's hilarious!")
+            .exit())
+
+@check50.check(exists)
+def test_edge_case_empty_input():
+    """handles empty input for noun, verb, adjective, and adverb"""
+    (check50.run("python3 madlib.py")
+            .stdin("")
+            .stdin("")
+            .stdin("")
+            .stdin("")
+            .stdout("Do you \s+ your \s+ \s+ \s+\? That's hilarious!", regex=True)
+            .exit())
+
+@check50.check(exists)
+def test_numerical_input():
+    """handles numerical input for noun, verb, adjective, and adverb"""
+    (check50.run("python3 madlib.py")
+            .stdin("123")
+            .stdin("456")
+            .stdin("789")
+            .stdin("0")
+            .stdout("Do you 456 your 789 123 0\? That's hilarious!", regex=True)
+            .exit())
---
+++ simple/__init__.py
@@ -0,0 +1,42 @@
+import check50
+
+@check50.check()
+def exists():
+    """simple.py exists"""
+    check50.exists("simple.py")
+
+@check50.check(exists)
+def test_valid_input():
+    """correctly performs arithmetic operations with valid inputs"""
+    (check50.run("python3 simple.py")
+            .stdin("10")
+            .stdin("5")
+            .stdout("10 \+ 5 = 15\n10 - 5 = 5\n10 \* 5 = 50\n10 / 5 = 2")
+            .exit())
+
+@check50.check(exists)
+def test_zero_division():
+    """handles division by zero appropriately"""
+    (check50.run("python3 simple.py")
+            .stdin("10")
+            .stdin("0")
+            .stdout("10 \+ 0 = 10\n10 - 0 = 10\n10 \* 0 = 0\n10 / 0 = [eE]rror", regex=True)
+            .exit())
+
+@check50.check(exists)
+def test_negative_numbers():
+    """correctly handles negative numbers"""
+    (check50.run("python3 simple.py")
+            .stdin("-5")
+            .stdin("3")
+            .stdout("-5 \+ 3 = -2\n-5 - 3 = -8\n-5 \* 3 = -15\n-5 / 3 = -1.6666667", regex=True)
+            .exit())
+
+@check50.check(exists)
+def test_non_numeric_input():
+    """handles non-numeric input gracefully"""
+    (check50.run("python3 simple.py")
+            .stdin("abc")
+            .stdin("5")
+            .stdout("[eE]rror", regex=True)
+            .exit())
