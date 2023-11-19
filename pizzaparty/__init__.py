5c91decee253ffe5d49b02208e8d44ce20a2ba79
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-11-19 15:30:21 +0400

    fix pizza party prompt

--- pizzaparty/__init__.py
+++ pizzaparty/__init__.py
@@ -9,9 +9,9 @@ def exists():
 def test_even_distribution():
     """correctly calculates even distribution of pizza slices"""
     (check50.run("python3 pizzaparty.py")
-            .stdin("8", prompt=False)
-            .stdin("2", prompt=False)
-            .stdin("8", prompt=False)
+            .stdin("8", prompt=True)
+            .stdin("2", prompt=True)
+            .stdin("8", prompt=True)
             .stdout("How many people\? 8\nHow many pizzas do you have\? 2\n8 people with 2 pizzas\nEach person gets 2 pieces of pizza.\nThere are 0 leftover pieces.")
             .exit(0))
 
@@ -19,9 +19,9 @@ def test_even_distribution():
 def test_leftover_slices():
     """handles leftover slices correctly"""
     (check50.run("python3 pizzaparty.py")
-            .stdin("5", prompt=False)
-            .stdin("2", prompt=False)
-            .stdin("6", prompt=False)
+            .stdin("5", prompt=True)
+            .stdin("2", prompt=True)
+            .stdin("6", prompt=True)
             .stdout("How many people\? 5\nHow many pizzas do you have\? 2\n5 people with 2 pizzas\nEach person gets 2 pieces of pizza.\nThere are 2 leftover pieces.")
             .exit(0))
 
@@ -29,8 +29,8 @@ def test_leftover_slices():
 def test_large_numbers():
     """handles large numbers correctly"""
     (check50.run("python3 pizzaparty.py")
-            .stdin("100", prompt=False)
-            .stdin("25", prompt=False)
-            .stdin("8", prompt=False)
+            .stdin("100", prompt=True)
+            .stdin("25", prompt=True)
+            .stdin("8", prompt=True)
             .stdout("How many people\? 100\nHow many pizzas do you have\? 25\n100 people with 25 pizzas\nEach person gets 2 pieces of pizza.\nThere are 0 leftover pieces.")
             .exit(0))
