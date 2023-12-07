d4e3b95bf1da665d882164a0a3a95e929295371f
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-12-07 17:26:07 +0400

    add password exercise

---
+++ password/__init__.py
@@ -0,0 +1,38 @@
+import check50
+
+@check50.check()
+def exists():
+    """password.py exists"""
+    check50.exists("password.py")
+
+@check50.check(exists)
+def test_correct_password():
+    """validates correct password"""
+    (check50.run("python3 password.py")
+            .stdin("abc$123")
+            .stdout("Welcome!", regex=False)
+            .exit(0))
+
+@check50.check(exists)
+def test_incorrect_password():
+    """rejects incorrect password"""
+    (check50.run("python3 password.py")
+            .stdin("12345")
+            .stdout("I don't know you.", regex=False)
+            .exit(0))
+
+@check50.check(exists)
+def test_case_sensitivity():
+    """ensures password validation is case sensitive"""
+    (check50.run("python3 password.py")
+            .stdin("ABC$123")
+            .stdout("I don't know you.", regex=False)
+            .exit(0))
+
+@check50.check(exists)
+def test_empty_password():
+    """handles empty password input"""
+    (check50.run("python3 password.py")
+            .stdin("")
+            .stdout("I don't know you.", regex=False)
+            .exit(0))
