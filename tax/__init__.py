1640ca4b6744372e6ce52b5d4b163ebb352c39b8
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-12-07 16:01:59 +0400

    add tax calculator

---
+++ tax/__init__.py
@@ -0,0 +1,60 @@
+import check50
+
+@check50.check()
+def exists():
+    """tax.py exists"""
+    check50.exists("tax.py")
+
+@check50.check(exists)
+def test_wisconsin_tax():
+    """calculates tax correctly for Wisconsin residents"""
+    (check50.run("python3 tax_calculator.py")
+            .stdin("50")
+            .stdin("WI")
+            .stdout("The subtotal is \$50.00.\nThe tax is \$2.75.\nThe total is \$52.75.", regex=True)
+            .exit(0))
+
+@check50.check(exists)
+def test_non_wisconsin():
+    """displays total without tax for non-Wisconsin residents"""
+    (check50.run("python3 tax_calculator.py")
+            .stdin("50")
+            .stdin("MN")
+            .stdout("The total is \$50.00", regex=True)
+            .exit(0))
+
+@check50.check(exists)
+def test_zero_order_amount():
+    """handles zero order amount"""
+    (check50.run("python3 tax_calculator.py")
+            .stdin("0")
+            .stdin("WI")
+            .stdout("The subtotal is \$0.00.\nThe tax is \$0.00.\nThe total is \$0.00.", regex=True)
+            .exit(0))
+
+@check50.check(exists)
+def test_negative_order_amount():
+    """handles negative order amount"""
+    (check50.run("python3 tax_calculator.py")
+            .stdin("-10")
+            .stdin("WI")
+            .stdout("", regex=False)
+            .exit(1))
+
+@check50.check(exists)
+def test_invalid_state_input():
+    """handles invalid state input"""
+    (check50.run("python3 tax_calculator.py")
+            .stdin("50")
+            .stdin("XYZ")
+            .stdout("", regex=False)
+            .exit(1))
+
+@check50.check(exists)
+def test_non_numeric_order_amount():
+    """handles non-numeric order amount"""
+    (check50.run("python3 tax_calculator.py")
+            .stdin("abc")
+            .stdin("WI")
+            .stdout("", regex=False)
+            .exit(1))
