13bb5cdf3476a8f338e36ce59986fb72952a23f8
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-20 15:05:08 +0400

    modify 59 - passgen task

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -1,5 +1,6 @@
 import check50
 import re
+import string
 
 @check50.check()
 def exists():
@@ -9,18 +10,18 @@ def exists():
 @check50.check(exists)
 def test_valid_input():
     """Valid input produces correct output."""
-    check = check50.run("python passgen.py").stdin("8").stdin("2").stdin("2")
-    output = check50.run("python passgen.py").stdin("8").stdin("2").stdin("2").stdout()
-    assert re.match(r"Your Password is [a-zA-Z0-9$@!#%&*]+", output), "Invalid password format"
+    check = check50.run("python passgen.py").stdin("8").stdin("2").stdin("2").exit(0)
+    output = check50.run("python passgen.py").stdin("8").stdin("2").stdin("2").stdout().exit(0)
+    assert re.match(r"Your Password is [a-zA-Z0-9#$%&'()*+,-./:;<=>?@^_`{|}~!]+", output), "Invalid password format"
 
-@check50.check(exists)
-def test_non_integer_input():
-    """Non-integer input prompts again."""
-    check = check50.run("python passgen.py").stdin("abc").stdin("def").stdin("ghi")
-    assert check.stdout() == "Please enter an integer.\n" * 3, "Non-integer input not handled properly"
+# @check50.check(exists)
+# def test_non_integer_input():
+#     """Non-integer input prompts again."""
+#     check = check50.run("python passgen.py").stdin("abc").stdin("def").stdin("ghi")
+#     assert check.stdout() == "Please enter an integer.\n" * 3, "Non-integer input not handled properly"
 
-@check50.check(exists)
-def test_zero_minimum_length():
-    """Minimum length of zero results in error."""
-    check = check50.run("python passgen.py").stdin("0").stdin("1").stdin("1")
-    assert check.stdout() == "Minimum length must be greater than zero.\n", "Minimum length zero not handled properly"
+# @check50.check(exists)
+# def test_zero_minimum_length():
+#     """Minimum length of zero results in error."""
+#     check = check50.run("python passgen.py").stdin("0").stdin("1").stdin("1")
+#     assert check.stdout() == "Minimum length must be greater than zero.\n", "Minimum length zero not handled properly"
