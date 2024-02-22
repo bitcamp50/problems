04c419919713055a20e984676c7a87e4650804a1
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-22 21:09:46 +0400

    passgen update

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -10,13 +10,13 @@ def exists():
 @check50.check(exists)
 def test_valid_input():
     """Valid input produces correct output."""
-    n1 = 2
-    n2 = 2
-    n3 = 8
+    n1 = '2'
+    n2 = '2'
+    n3 = '8'
     text = "12#Abcde"
     expected_output = test_regex(text, n1, n2, n3).pattern
     check50.run("python passgen.py").stdin(n3).stdin(n2).stdin(n1).stdout(expected_output, regex=True).exit()
     
 def test_regex(txt, n1,n2,n3):
-    pattern = re.compile(fr'^(?=.*\d{{{n1}}})(?=.*[\W_]{{{n2}}})[A-Za-z\d\W_]{{{n3}}}$')
+    pattern = re.compile(fr'^(?=.*\d{{{int(n1)}}})(?=.*[\W_]{{{int(n2)}}})[A-Za-z\d\W_]{{{int(n3)}}}$')
     return pattern
