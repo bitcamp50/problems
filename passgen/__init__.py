30c22c8073bd91b3bae53cde74f02aa71196e7cc
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-22 20:40:56 +0400

    passgen update

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -10,10 +10,16 @@ def exists():
 @check50.check(exists)
 def test_valid_input():
     """Valid input produces correct output."""
-    check = check50.run("python passgen.py").stdin("8").stdin("2").stdin("2").exit(0)
-    output = check50.run("python passgen.py").stdin("8").stdin("2").stdin("2").stdout().exit(0)
-    assert re.match(r"Your Password is [a-zA-Z0-9#$%&'()*+,-./:;<=>?@^_`{|}~!]+", output), "Invalid password format"
-
+    # check = check50.run("python passgen.py").stdin("8").stdin("2").stdin("2").exit(0)
+    n1 = 2
+    n2 = 2
+    n3 = 8
+    text = "12#Abcde"
+    check50.run("python passgen.py").stdin(n3).stdin(n2).stdin(n1).stdout(test_regex(text, n1,n2,n3),text,regex=True).exit()
+    
+def test_regex(txt, n1,n2,n3):
+    pattern = re.compile(fr'^(?=.*\d{{{n1}}})(?=.*[\W_]{{{n2}}})[A-Za-z\d\W_]{{{n3}}}$')
+    return pattern.match(txt)
 # @check50.check(exists)
 # def test_non_integer_input():
 #     """Non-integer input prompts again."""
