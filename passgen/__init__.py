ea661e7c274db32588ff8d2b0bd52ee2573e8a1b
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-22 20:54:50 +0400

    passgen update

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -10,24 +10,13 @@ def exists():
 @check50.check(exists)
 def test_valid_input():
     """Valid input produces correct output."""
-    # check = check50.run("python passgen.py").stdin("8").stdin("2").stdin("2").exit(0)
     n1 = 2
     n2 = 2
     n3 = 8
     text = "12#Abcde"
-    check50.run("python passgen.py").stdin(n3).stdin(n2).stdin(n1).stdout(test_regex(text, n1,n2,n3),text,regex=True).exit()
+    expected_output = test_regex(text, n1, n2, n3)
+    check50.run("python passgen.py").stdin(n3).stdin(n2).stdin(n1).stdout(expected_output, regex=True).exit()
     
 def test_regex(txt, n1,n2,n3):
     pattern = re.compile(fr'^(?=.*\d{{{n1}}})(?=.*[\W_]{{{n2}}})[A-Za-z\d\W_]{{{n3}}}$')
     return pattern.match(txt)
-# @check50.check(exists)
-# def test_non_integer_input():
-#     """Non-integer input prompts again."""
-#     check = check50.run("python passgen.py").stdin("abc").stdin("def").stdin("ghi")
-#     assert check.stdout() == "Please enter an integer.\n" * 3, "Non-integer input not handled properly"
-
-# @check50.check(exists)
-# def test_zero_minimum_length():
-#     """Minimum length of zero results in error."""
-#     check = check50.run("python passgen.py").stdin("0").stdin("1").stdin("1")
-#     assert check.stdout() == "Minimum length must be greater than zero.\n", "Minimum length zero not handled properly"
