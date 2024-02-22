c3d7fa370a67f277f40b42ef1663d59515015181
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-22 21:07:49 +0400

    passgen update

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -14,7 +14,7 @@ def test_valid_input():
     n2 = 2
     n3 = 8
     text = "12#Abcde"
-    expected_output = test_regex(text, n1, n2, n3)
+    expected_output = test_regex(text, n1, n2, n3).pattern
     check50.run("python passgen.py").stdin(n3).stdin(n2).stdin(n1).stdout(expected_output, regex=True).exit()
     
 def test_regex(txt, n1,n2,n3):
