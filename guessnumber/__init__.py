de08305bab14a9348dd4ee99276a9625f65cc7e7
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-02-23 16:15:10 +0400

    comment the broken checks

--- guessnumber/__init__.py
+++ guessnumber/__init__.py
@@ -7,32 +7,15 @@ def exists():
     # This line is conceptual, assuming testing.py is prepared for testing
     check50.include("testing.py")
 
-@check50.check(exists)
-def test_level_1_fixed_guess():
-    """Level 1 gameplay with fixed guess works"""
-    check50.run("python3 testing.py")\
-        .stdin("1")\
-        .stdin("5")\
-        .stdout("You got it in 1 guess!", regex=True)\
-        .exit(0)
-
-@check50.check(exists)
-def test_level_2_fixed_guess():
-    """Level 2 gameplay with fixed guess works"""
-    check50.run("python3 testing.py")\
-        .stdin("2")\
-        .stdin("50")\
-        .stdout("You got it in 1 guess!", regex=True)\
-        .exit(0)
-
-@check50.check(exists)
-def test_level_3_fixed_guess():
-    """Level 3 gameplay with fixed guess works"""
-    check50.run("python3 testing.py")\
-        .stdin("3")\
-        .stdin("500")\
-        .stdout("You got it in 1 guess!", regex=True)\
-        .exit(0)
+# @check50.check(exists)
+# def test_level_1_fixed_guess():
+#     """Level 1 gameplay with fixed guess works"""
+#     check50.run("python3 testing.py")\
+#         .stdin("1")\
+#         .stdin("5")\
+#         .stdout("You got it in 1 guess!", regex=True)\
+#         .stdin("n")\
+#         .exit(0)
 
 @check50.check(exists)
 def test_invalid_input_guess():
@@ -46,20 +29,20 @@ def test_invalid_input_guess():
         .stdin("n")\
         .exit(0)
 
-@check50.check(exists)
-def test_play_again_yes_then_no():
-    """Player chooses to play again and then chooses not to"""
-    check50.run("python3 testing.py")\
-        .stdin("1")\
-        .stdin("5", prompt=True)\
-        .stdout("You got it in 1 guess!", regex=True)\
-        .stdin("y", prompt=True)\
-        .stdin("1")\
-        .stdin("5", prompt=True)\
-        .stdout("You got it in 1 guess!", regex=True)\
-        .stdin("n", prompt=True)\
-        .stdout("Goodbye!", regex=True)\
-        .exit(0)
+# @check50.check(exists)
+# def test_play_again_yes_then_no():
+#     """Player chooses to play again and then chooses not to"""
+#     check50.run("python3 testing.py")\
+#         .stdin("1")\
+#         .stdin("5", prompt=True)\
+#         .stdout("You got it in 1 guess!", regex=True)\
+#         .stdin("y", prompt=True)\
+#         .stdin("1")\
+#         .stdin("5", prompt=True)\
+#         .stdout("You got it in 1 guess!", regex=True)\
+#         .stdin("n", prompt=True)\
+#         .stdout("Goodbye!", regex=True)\
+#         .exit(0)
 
 def regex(pattern):
     """Match case-insensitively with any characters on either side"""
