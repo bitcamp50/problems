39edf868ae9283663abeada96da7968319c29ad3
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-27 06:01:27 +0400

    modified magicball

--- magicball/__init__.py
+++ magicball/__init__.py
@@ -18,7 +18,7 @@ def test_valid_input_question():
 def test_invalid_input_question():
     """Handles question input without '?' character"""
     question = "I will not rich\n"
-    check = check50.run("python3 magicball.py")
+    check = check50.run("python3 testing.py")
     check.stdin(question)
     check.stdout("This is not a question")
     check.stdin("Will I be rich and famous?\n")
