30d3bd10802bc0ab3c2e5472c861148e3ff99305
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-27 04:23:14 +0400

    modified passgen

--- passgen/testing.py
+++ passgen/testing.py
@@ -4,19 +4,27 @@ import string
 
 # Save the original randint function
 _original_choices = random.choices
-fixed_letters = list(string.ascii_letters)
-fixed_digits = list(string.digits)
-fixed_punctuation = list(string.punctuation)
-# Override the randint function to return a fixed value
-def _fixed_choices(population, k):
-    if population == string.ascii_letters:
-        return random.choices(fixed_letters, k=k)
-    elif population == string.digits:
-        return random.choices(fixed_digits, k=k)
-    elif population == string.punctuation:
-        return random.choices(fixed_punctuation, k=k)
+# fixed_letters = list(string.ascii_letters)
+# fixed_digits = list(string.digits)
+# fixed_punctuation = list(string.punctuation)
+# # Override the randint function to return a fixed value
+# def _fixed_choices(population, k):
+#     if population == string.ascii_letters:
+#         return random.choices(fixed_letters, k=k)
+#     elif population == string.digits:
+#         return random.choices(fixed_digits, k=k)
+#     elif population == string.punctuation:
+#         return random.choices(fixed_punctuation, k=k)
 
-random.choices = _fixed_choices
+
+def mock_choices(population, k):
+    if population == string.ascii_letters:
+        return ['h', 'b', 'O'] * (k // 3) + ['h'] * (k % 3)  # Mimicking the behavior of random.choices
+    elif population == string.digits:
+        return ['2', '3']
+    elif population == string.punctuation:
+        return [':', '!']
+random.choices = mock_choices
 
 # Import the game script after overriding randint
 import passgen
