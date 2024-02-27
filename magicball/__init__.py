7ef71c208811c0f153fb8af705eca0db26dd5be9
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-27 05:57:21 +0400

    modified magicball

--- magicball/__init__.py
+++ magicball/__init__.py
@@ -16,17 +16,17 @@ def test_valid_input_question():
 
 @check50.check(exists)
 def test_invalid_input_question():
-    """Handles question input with '?' character"""
+    """Handles question input without '?' character"""
     question = "I will not rich"
-    if question.endswith("?"):
-        check50.run("python3 testing.py")\
-            .stdin("Will I be rich and famous?")\
-            .stdout("Yes", regex=False)\
-            .exit(0)
-    else:
-        check50.run("python3 magicball.py")\
-            .stdin("Will I be rich and famous")\
-            .stdout("This is not a question", regex=False)\
-            .exit(0)
+    check = check50.run("python3 magicball.py")
+    check.stdin(question)
+    while True:
+        output = check.stdout()
+        if "This is not a question" in output:
+            check.stdin("Will I be rich and famous?\n")
+        elif "Yes" in output:
+            break
+        else:
+            raise check50.Failure("Unexpected output: {}".format(output))
         
 
