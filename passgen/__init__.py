49b7ad3d5ac7cc7600e72056d78dae550473694d
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-27 05:04:28 +0400

    modified passgen

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -1,6 +1,4 @@
 import check50
-import re
-import string
 import random
 
 @check50.check()
@@ -9,35 +7,8 @@ def exists():
     check50.exists("passgen.py")
     check50.include("testing.py")
 
-# @check50.check(exists)
-# def test_valid_input():
-#     """Valid input produces correct output."""
-#     n1 = '2'
-#     n2 = '2'
-#     n3 = '8'
-#     pattern = re.compile(fr'^(?=.*\d{{{int(n1)}}})(?=.*[\W_]{{{int(n2)}}})[A-Za-z\d\W_]{{{int(n3)}}}$')
-#     random.seed(42)
-#     # Generate a random password
-#     password = ''.join(random.choices(string.ascii_letters, k=4) + random.choices(string.digits,k=2) + random.choices(string.punctuation,k=2))
-    
-#     # Check if the generated password matches the specified pattern
-#     if pattern.match(password):
-#         expected_output = f"Your password is\n{password}"
-#     else:
-#         expected_output = None
-
-#     # Run the program and validate the output
-#     if expected_output is not None:
-#         check50.run("python passgen.py").stdin(n3).stdin(n2).stdin(n1).stdout(expected_output, regex=True).exit()
-#     else:
-#         check50.run("python passgen.py").stdin(n3).stdin(n2).stdin(n1).exit()
-
 @check50.check(exists)
 def test_valid_input():
     
     result = f"Your password is\nabc@!12"
     check50.run("python3 testing.py").stdin("7").stdin("2").stdin("2").stdout(result, regex=False).exit()
-
-def regex(pattern):
-    """Match case-insensitively with any characters on either side"""
-    return fr'{pattern}'
--- passgen/testing.py
+++ passgen/testing.py
@@ -2,19 +2,8 @@ import random
 import sys
 import string
 
-# Save the original randint function
+
 _original_choices = random.choices
-# fixed_letters = list(string.ascii_letters)
-# fixed_digits = list(string.digits)
-# fixed_punctuation = list(string.punctuation)
-# # Override the randint function to return a fixed value
-# def _fixed_choices(population, k):
-#     if population == string.ascii_letters:
-#         return random.choices(fixed_letters, k=k)
-#     elif population == string.digits:
-#         return random.choices(fixed_digits, k=k)
-#     elif population == string.punctuation:
-#         return random.choices(fixed_punctuation, k=k)
 
 
 def mock_choices(population, k):
@@ -30,9 +19,9 @@ def mock_shuffle(items):
     return items 
 random.choices = mock_choices
 random.shuffle = mock_shuffle
-# Import the game script after overriding randint
+
 import passgen
 
-# Ensure the main function of guessnumber.py runs when guessnumber_testing.py is executed
+
 if __name__ == "__main__":
     passgen.main()
