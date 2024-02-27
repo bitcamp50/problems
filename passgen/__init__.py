c214343d9e3b5689a125781f90574a09fd8b22dd
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-27 04:47:47 +0400

    modified passgen

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -35,8 +35,8 @@ def exists():
 @check50.check(exists)
 def test_valid_input():
     
-    result = f"Your password is\nObh!:23"
-    check50.run("python3 testing.py").stdin("7").stdin("2").stdin("2").stdout(regex(result), regex=True).exit(0)
+    result = f"Your password is\na!a1b2c"
+    check50.run("python3 testing.py").stdin("7").stdin("2").stdin("2").stdout(result, regex=Flase).exit(0)
 
 def regex(pattern):
     """Match case-insensitively with any characters on either side"""
--- passgen/testing.py
+++ passgen/testing.py
@@ -19,11 +19,11 @@ _original_choices = random.choices
 
 def mock_choices(population, k):
     if population == string.ascii_letters:
-        return 'Obh' # Fixed characters for letters
+        return ['a', 'b', 'c'] * (k // 3) + ['a'] * (k % 3) # Fixed characters for letters
     elif population == string.digits:
-        return '23' # Fixed characters for digits
+        return ['1', '2'] * (k // 2) # Fixed characters for digits
     elif population == string.punctuation:
-        return '!:' # Fixed characters for punctuation
+        return ['@', '!'] * (k // 2) # Fixed characters for punctuation
 
 
 def mock_shuffle(items):
