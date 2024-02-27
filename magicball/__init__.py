8552defc3c7d6343c9cf53904288c7ce06db27ea
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-27 06:08:10 +0400

    modified magicball

--- magicball/__init__.py
+++ magicball/__init__.py
@@ -7,13 +7,29 @@ def exists():
     check50.include("testing.py")
 
 @check50.check(exists)
-def test_valid_input_question():
-    """Handles question input with '?' character"""
+def test_valid_input_question_Y():
+    """Handles question input with '?' character and answer Yes"""
     check50.run("python3 testing.py")\
         .stdin("Will I be rich and famous?")\
         .stdout("Yes", regex=False)\
         .exit(0)
 
+@check50.check(exists)
+def test_valid_input_question_N():
+    """Handles question input with '?' character and answer No"""
+    check50.run("python3 testing2.py")\
+        .stdin("Will I be famous?")\
+        .stdout("No", regex=False)\
+        .exit(0)
+
+@check50.check(exists)
+def test_valid_input_question_N():
+    """Handles question input with '?' character and answer Ask again later"""
+    check50.run("python3 testing3.py")\
+        .stdin("Will I be famous?")\
+        .stdout("Ask again later", regex=False)\
+        .exit(0)
+    
 @check50.check(exists)
 def test_invalid_input_question():
     """Handles question input without '?' character"""
