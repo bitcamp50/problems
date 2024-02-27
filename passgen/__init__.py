f02921d3847091eb6e3f6639b8d1075f6622be28
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-27 07:05:10 +0400

    modified passgen

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -6,19 +6,35 @@ def exists():
     """password_generator.py exists."""
     check50.exists("passgen.py")
     check50.include("testing.py")
+    check50.include("testing2.py")
 
 @check50.check(exists)
 def test_valid_input():
     """Test valid inputs"""
     result = f"Your password is\nabc@!12"
-    check50.run("python3 testing.py").stdin("7").stdin("2").stdin("2").stdout(result, regex=False).exit()
+    check50.run("python3 testing.py")\
+        .stdin("7")\
+        .stdin("2")\
+        .stdin("2")\
+        .stdout(result, regex=False).exit()
+    
+@check50.check(exists)
+def test_valid_input_2():
+    """Test valid inputs again"""
+    result = f"Your password is\nNoKiA?.@!2024"
+    check50.run("python3 testing2.py")\
+        .stdin("13")\
+        .stdin("4")\
+        .stdin("4")\
+        .stdout(result, regex=False).exit()
 
 @check50.check(exists)
 def test_invalid_input():
     """Test invalid inputs"""
     result = f"Your password is\nabc@!12"
     check50.run("python3 testing.py")\
-    .stdin("cat").stdin("dog")\
+    .stdin("cat")\
+    .stdin("dog")\
     .stdin("table")\
     .stdin("7")\
     .stdin("2")\
