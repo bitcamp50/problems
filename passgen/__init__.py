b61c67ec1b8e32ec73b43a194b1d850d2a55d842
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-27 06:58:10 +0400

    modified passgen

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -9,6 +9,18 @@ def exists():
 
 @check50.check(exists)
 def test_valid_input():
-    
+    """Test valid inputs"""
     result = f"Your password is\nabc@!12"
     check50.run("python3 testing.py").stdin("7").stdin("2").stdin("2").stdout(result, regex=False).exit()
+
+@check50.check(exists)
+def test_invalid_input():
+    """Test invalid inputs"""
+    result = f"Your password is\nabc@!12"
+    check50.run("python3 testing.py")\
+    .stdin("cat").stdin("dog")\
+    .stdin("table")\
+    .stdin("7")\
+    .stdin("2")\
+    .stdin("2")\
+    .stdout(result, regex=False).exit()
