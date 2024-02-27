cc6115a4a81ad1565aa2d1fac72d3a088732fd02
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-02-27 05:28:29 +0400

    modified magicball

--- magicball/testing.py
+++ magicball/testing.py
@@ -2,18 +2,18 @@ import random
 import sys
 
 # Save the original randint function
-_original_randint = random.randint
+_original_randint = random.choice
 
 # Override the randint function to return a fixed value
-def _fixed_randint(start, end):
+def _fixed_choice(start, end):
     # You can adjust this value based on the testing scenario
-    return 5
+    return "Yes"
 
-random.randint = _fixed_randint
+random.randint = _fixed_choice
 
 # Import the game script after overriding randint
-import guessnumber
+import magicball
 
 # Ensure the main function of guessnumber.py runs when guessnumber_testing.py is executed
 if __name__ == "__main__":
-    guessnumber.main()
+    magicball.main()
