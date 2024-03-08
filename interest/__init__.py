0d2b85f15f5d29fb270198c7fcbed4fba8d8e795
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-03-08 18:15:35 +0400

    add checks for interest exercise

---
+++ interest/__init__.py
@@ -0,0 +1,25 @@
+import check50
+
+@check50.check()
+def exists():
+    """Test if interest.py exists."""
+    check50.exists("test_interest.py")
+    check50.exists("interest.py")
+
+
+@check50.check(exists)
+def test_investment():
+    """Test calculation of the investment"""
+    check50.run("python3 interest.py")\
+        .stdin("1500")\
+        .stdin("4.3")\
+        .stdin("4")\
+        .stdout("After 4 years at 4.3%, the investment will\nbe worth $1758.", regex=False)\
+        .exit(0)
+
+@check50.check(exists)
+def test_value_error():
+    """Test for a string as an input"""
+    check50.run("python3 interest.py")\
+        .stdin("asdf")\
+        .exit(1)
