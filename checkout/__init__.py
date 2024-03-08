74ef26dcf0192213d1d1e526526fd29bc6c79327
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-03-08 16:54:40 +0400

    adding some checks to the checkout

--- checkout/__init__.py
+++ checkout/__init__.py
@@ -8,7 +8,7 @@ def exists():
 
 @check50.check(exists)
 def test_subtotal():
-    """Test if tax is calculated correctly."""
+    """Test if values are calculated correctly."""
     check50.run("python3 checkout.py")\
         .stdin("1")\
         .stdin("1")\
@@ -18,40 +18,29 @@ def test_subtotal():
         .stdin("5")\
         .stdout("Subtotal: $27.00\nTax: $1.49\nTotal: $28.48", regex=False)\
         .exit(0)
-    
-    
-# @check50.check(exists)
-# def test_tax():
-#     """Test if tax is calculated correctly."""
-#     check50.run("python checkout.py", input="25 2 10 1 4 1", capture_output=True)
-#     output = check50.get_output()
-    
-#     expected_tax = "$3.52"
-#     assert expected_tax in output, f"Missing or incorrect tax. Expected: {expected_tax}"
 
+@check50.check(exists)
+def test_subtotal():
+    """Test if values are calculated correctly 2"""
+    check50.run("python3 checkout.py")\
+        .stdin("25")\
+        .stdin("2")\
+        .stdin("10")\
+        .stdin("1")\
+        .stdin("4")\
+        .stdin("1")\
+        .stdout("Subtotal: $64.00\nTax: $3.52\nTotal: $67.52", regex=False)\
+        .exit(0)
 
-# @check50.check(exists)
-# def test_total():
-#     """Test if total is calculated correctly."""
-#     check50.run("python checkout.py", input="25 2 10 1 4 1", capture_output=True)
-#     output = check50.get_output()
-    
-#     expected_total = "$67.52"
-#     assert expected_total in output, f"Missing or incorrect total. Expected: {expected_total}"
-
-
-
-# @check50.check(exists)
-# def test_formatting():
-#     """Test if output is formatted correctly."""
-#     check50.run("python checkout.py", input="25 2 10 1 4 1", capture_output=True)
-#     output = check50.get_output()
-    
-#     for line in output.splitlines():
-#         if line.startswith("Enter"):
-#             continue
-        
-#         parts = line.split()
-#         assert len(parts) == 3, f"Incorrect line format: {line}"
-        
-#         assert parts[0].isdigit() and parts[1].isdigit(), f"Invalid quantity or price format: {line}"
+@check50.check(exists)
+def test_subtotal():
+    """Test if values are calculated correctly 3"""
+    check50.run("python3 checkout.py")\
+        .stdin("16")\
+        .stdin("4")\
+        .stdin("3")\
+        .stdin("54")\
+        .stdin("1")\
+        .stdin("2")\
+        .stdout("Subtotal: $228.00\nTax: $12.54\nTotal: $240.54", regex=False)\
+        .exit(0)
