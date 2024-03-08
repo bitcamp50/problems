1c6b3c3e69eee8a08a3d3cb42baf30729b672588
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-03-08 18:17:53 +0400

    add second test for the interset exercise

--- interest/__init__.py
+++ interest/__init__.py
@@ -16,6 +16,17 @@ def test_investment():
         .stdin("4")\
         .stdout("After 4 years at 4.3%, the investment will\nbe worth $1758.", regex=False)\
         .exit(0)
+    
+
+@check50.check(exists)
+def test_investment_values():
+    """Test calculation of the investment with different inputs"""
+    check50.run("python3 interest.py")\
+        .stdin("1300")\
+        .stdin("2.3")\
+        .stdin("6")\
+        .stdout("After 6 years at 2.3%, the investment will\nbe worth $1479.", regex=False)\
+        .exit(0)
 
 @check50.check(exists)
 def test_value_error():
