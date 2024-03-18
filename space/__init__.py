9d6a0de829b385febf9db09701a44272608e288f
Lasha101 <gurshabo55@gmail.com>
2024-03-18 12:14:16 +0400

    add corrected space.py

--- space/__init__.py
+++ space/__init__.py
@@ -8,12 +8,12 @@ def exists():
 @check50.check(exists)
 def test_json_response():
     """space.py makes a successful request and parses JSON response"""
-    result = check50.run("python3 space.py").stdout("API request successful").stdout("JSON parsing successful").exit(0)
+    check50.run("python3 space.py").stdout("<Response [200]>").exit(0)
 
 @check50.check(exists)
 def test_output_format():
     """space.py prints the correct output format"""
-    result = check50.run("python3 space.py").stdout("{:<20}| {:<20}".format("name", "craft")).stdout("-" * 30)
+    result = check50.run("python3 space.py")#.stdout("{:<20}| {:<20}".format("name", "craft")).stdout("-" * 30)
 
     # Add more specific checks for names and crafts
     result.stdout("Jasmin Moghbeli").stdout("ISS")
