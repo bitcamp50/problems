f4616378d7e6cce08c2b8aa0dd5887248cc8cb45
Lasha101 <gurshabo55@gmail.com>
2024-03-19 22:25:29 +0400

    add corrected weather.py

--- weather/__init__.py
+++ weather/__init__.py
@@ -10,7 +10,7 @@ def exists():
 @check50.check(exists)
 def test_json_response():
     """weather.py makes a successful request and parses JSON response"""
-    city = "Tbilisi"
+    city = "tbilisi"
     api_key = "45fca6311fa8a41b61aee922ec47c231"
     base_url = "http://api.openweathermap.org/data/2.5/weather"
     params = {
@@ -25,6 +25,6 @@ def test_json_response():
     
     expected_output = f"Temperature in {city} is: {temperature}"
     
-    result = check50.run("python3 weather.py")
+    result = check50.run("python3 weather.py").stdin("tbilisi")
     result.stdout(expected_output, regex=False)
     result.exit(0)
